8c61e57f00f5c716af32924532605dacc89b9c05
Anna Bieliaieva <belyaevaan80@gmail.com>
2023-07-17 20:13:15 +0200

    add styles

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ const container = document.getElementById('container');
 //TODO  stule by class
 
 picturesButton.addEventListener('click',()=>{
+
+const title = document.createElement('p');
+title.textContent = 'This pictures you can see for 5 sek!'
+container.append(title);
+
 const pictures = document.createElement('img');
 pictures.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zZ-F3BDDAh1423MOtzP1f9YR7_yChau_eA&usqp=CAU";
-
 pictures.classList.add('picturesImg');
-
 container.append(pictures);
 
 
-setTimeout(()=>{pictures.remove()},5000);
+setTimeout(()=>{
+    pictures.remove();
+    title.remove();
+},5000);
 
 })
 
